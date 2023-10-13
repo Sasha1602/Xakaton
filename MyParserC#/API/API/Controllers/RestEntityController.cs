45849c35cdaf3e2537b154574b25ce6fa7d341a7
Fuflick <stepan_belov_2016@mail.ru>
2023-10-13 19:18:59 +0300

    Fuck, we need parser again, begin to write API

---
+++ MyParserC#/API/API/Controllers/RestEntityController.cs
@@ -0,0 +1,26 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RestEntityController : ControllerBase
+{
+
+    public MyDbContext _dbContext = new MyDbContext();
+
+    [HttpPost]
+    public void PostRequest(string body)
+    {
+        var response = new BsonElement("response", new BsonString(body));
+        var collection = _dbContext.Client.GetDatabase("XakaDB").GetCollection<BsonDocument>("generatedPics");
+        collection.Add
+    }
+    
+}
+
+
+    
