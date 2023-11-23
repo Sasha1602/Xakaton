0a5db0a67f052ea6d7cfcd3142fe6c93650af8a4
Fuflick <stepan_belov_2016@mail.ru>
2023-11-23 10:02:37 +0300

    Add pre UserEntity class and pre MyDbContext class

---
+++ Server/ParserAndMore/DAL/MyDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL;
+
+public class MyDbContext : DbContext //ToDo: migrate to MySql database
+{
+    public DbSet<UserEntity> Users = null!;
+}
---
+++ Server/ParserAndMore/Domain/UserEntity.cs
@@ -0,0 +1,35 @@
+﻿namespace Domain;
+
+public class UserEntity
+{
+    private string _password;
+
+    private string _login;
+
+    public string Password
+    {
+        get
+        {
+            return _password;
+        }
+        set
+        {
+            _password = value;
+        }
+    }
+    
+    public string Login
+    {
+        get
+        {
+            return _login;
+        }
+
+        set
+        {
+            _login = value;
+        }
+    }
+    
+    public Guid Id { get; private set; }
+}
