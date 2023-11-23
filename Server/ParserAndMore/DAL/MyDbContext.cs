5cced26e19c7804473895296a7571f89c78283e4
Fuflick <stepan_belov_2016@mail.ru>
2023-11-23 10:08:14 +0300

    add some properties to MyDbContext, need to download Connector

--- Server/ParserAndMore/DAL/MyDbContext.cs
+++ Server/ParserAndMore/DAL/MyDbContext.cs
@@ -6,4 +6,12 @@ namespace DAL;
 public class MyDbContext : DbContext //ToDo: migrate to MySql database
 {
     public DbSet<UserEntity> Users = null!;
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql("server=localhost;user=root;password=123456789;database=usersdb;", 
+            // ToDo: download MySQL Connector for Dotnet and change MySqlServerVersion() func    
+            new MySqlServerVersion(new Version(8, 0, 25))); 
+         
+    }
 }
