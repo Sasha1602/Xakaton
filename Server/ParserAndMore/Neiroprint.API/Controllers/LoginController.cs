c732018ebe44aceb574e79dbcaac51b062c0a1dc
Fuflick <stepan_belov_2016@mail.ru>
2023-12-19 11:04:19 +0300

    Some fixes of LoginController

--- Server/ParserAndMore/Neiroprint.API/Controllers/LoginController.cs
+++ Server/ParserAndMore/Neiroprint.API/Controllers/LoginController.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Domain;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Controllers
@@ -12,17 +7,18 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
-        List<UserEntity> persons = new List<UserEntity>
+        private List<UserEntity> _persons = new List<UserEntity>
         {
-            new UserEntity {Login = "admin", Password = "admin"},
-            new UserEntity { Login = "123", Password = "123"},
+            new UserEntity {Login = "admin", Password = "admin", Id = "1"},
+            new UserEntity { Login = "123", Password = "123", Id = "2"},
         };
 
         [HttpGet]
         [Route("api/GetAllUsers")]
         public async Task GetAllUsers()
-        {   
-            await Response.WriteAsJsonAsync(persons);
+        {
+            var collection = _persons;
+            await Response.WriteAsJsonAsync(collection);
         }
 
         [HttpPost]
@@ -31,24 +27,47 @@ namespace WebApplication1.Controllers
         {
             try
             {
+                var user = _persons.FirstOrDefault(user => user.Login == person.Login);
+                
                 // check request params
-                if (person != null)
+                if (user is null)
                 {
                     // set new user's Id
                     person.Id = Guid.NewGuid().ToString();
                     // add user to database
-                    persons.Add(person);
+                    _persons.Add(person);
                     await Response.WriteAsJsonAsync(person);
                 }
                 else
                 {
-                    throw new Exception("Not correct data");
+                    await Response.WriteAsJsonAsync(new { message = "Not correct data" });
                 }
             }
             catch (Exception)
             {
                 Response.StatusCode = 400;
                 await Response.WriteAsJsonAsync(new { message = "Not correct data" });
+            }
+        }
+        
+        [HttpDelete]
+        [Route("DeleteUser")]
+        public async Task DeleteUser([FromBody] string? id)
+        {
+            // Check current Id is exist
+            var user = _persons.FirstOrDefault(u => u.Id == id);
+            {
+                if (user != null)
+                {
+                    //Delete user from db
+                    _persons.Remove(user);
+                    await Response.WriteAsJsonAsync(user);
+                }
+                else
+                {
+                    Response.StatusCode = 404;
+                    await Response.WriteAsJsonAsync(new {mewssage = "User not found."});
+                }
             }
         }
     }
