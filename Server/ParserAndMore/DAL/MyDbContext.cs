1cb61337101f5818da95ca462e5726a0506dfd28
Fuflick <stepan_belov_2016@mail.ru>
2024-02-08 03:43:58 +0300

    try to connect to mysql

--- Server/ParserAndMore/DAL/MyDbContext.cs
+++ Server/ParserAndMore/DAL/MyDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Domain;
 using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
 
 namespace DAL;
 
@@ -14,15 +15,24 @@ public class MyDbContext : DbContext //ToDo: migrate to MySql database
     public DbSet<ResponseEntity> Responses = null!; //for history of requests realization
 
     public DbSet<RequestEntity> Requsets = null!;
-    
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=Neiroprint;UserName=postgres;Password=1234;");
 
-        /*("server=localhost;user=root;password=123456789;database=usersdb;",
-        // ToDo: download MySQL Connector for Dotnet and change MySqlServerVersion() func
-        new MySqlServerVersion(new Version(8, 0, 25)));*/
-        //This for MySql
+    static string connectionString = "server=localhost;port=3306;database=Neuroprint;uid=root;password=1234";
+    MySqlConnection connection = new MySqlConnection(connectionString);
+    
+     protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        try
+        {
+            connection.Open();
+            Console.WriteLine("Соединение с MySQL установлено!");
+        }
+        catch (MySqlException ex)
+        {
+            Console.WriteLine("Ошибка подключения: " + ex.Message);
+        }
 
     }
+
+
+
 }
